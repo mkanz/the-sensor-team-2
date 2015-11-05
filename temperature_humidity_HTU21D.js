18d71993da40e7281a58a7bce0550f3655cd7da1
root <root@beaglebone.localdomain>
2015-11-05 06:54:07 +0000

    Modified to give stable readings
    Also added comments, made code user friendly

--- temperature_humidity_HTU21D.js
+++ temperature_humidity_HTU21D.js
@@ -1,27 +1,55 @@
+/* Get Temperature and humidity values from the Sparkfun
+ * HTU21D sensor. Connections are shown in the 'HTU21D_connections.png'
+ * fritzing diagram. 
+ *
+ * Tested on the Beaglebone Black.
+ *
+ * 05 Nov 2015
+ * 
+ * Original version:
+ * 		Kansul Mahrifa
+ *
+ * Edits:
+ *		Ahmed Karanath
+ *		Thejas Babu
+ */
+
 var b = require('bonescript');
 var port  = '/dev/i2c-2';		// Using bus 2
-var gy521 = 0x40;
-var ms    = 500;				// Repeat time in ms
+var htu21d = 0x40;
+var ms    = 1000;				// Repeat time in ms
 
-b.i2cOpen(port, gy521);
+b.i2cOpen(port, htu21d);
 
-b.i2cWriteBytes(port, 0xe7, [0]); // To Wake up the HTU21D
+b.i2cWriteBytes(port, 0xE7, [0]); // Turn on the HTU21D
 
-setInterval(readData, ms)
+setInterval(readData, ms);
 
 function readData() {
 	// i2cReadBytes(port, command, length, [callback])
-	b.i2cReadBytes(port, 0xe3, 4, displayData);
+	
+	b.i2cReadBytes(port, 0xE3, 2, displayTemp);			//Read Temperature
+	b.i2cReadBytes(port, 0xE3, 2, displayHumid);		//Read Humidity
 }
 
-function displayData(x) {
-	var data;
+function displayTemp(x) {
 	if(x.event === 'return') {
-		data = {
-			temperature:  (((x.return[0]<<8 | x.return[1])<<16)>>16), // Raw temperature
-			humidity:  ((((x.return[2]<<8 | x.return[3])<<16)>>16)) //Raw humidity
-		};
+		var raw_temperature = (((x.return[0]<<8 | x.return[1])<<16)>>16); // Raw temperature
+		
+		//Converting to temperature in degree Celcius
+		var tempSigTemp = raw_temperature / 65536; //2^16 = 65536
+  		var temperature_in_c = (-46.85 + (175.72 * tempSigTemp)).toFixed(2); //From documentation in Sparkfun website
+		console.log('temp in C: '+ temperature_in_c);
+	}
+}
+
+function displayHumid(x) {
+	if(x.event === 'return') {
+		var raw_humidity = ((((x.return[0]<<8 | x.return[1])<<16)>>16)) //Raw humidity
 	
-		console.log(data);
+		//Convert to relative humidity percent
+		var tempSigRH = raw_humidity / 65536; //2^16 = 65536
+  		var rel_humidity_percent = (-6 + (125 * tempSigRH)).toFixed(2); ////From documentation in Sparkfun website
+  		console.log('Rel humidity: ' + rel_humidity_percent + ' %\n');
 	}
 }
